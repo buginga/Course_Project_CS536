4f100f8ab01dcc7cef2c1719660e422c9e71ad20
AuroraWXZ <wang6171@purdue.edu>
2024-11-20 22:35:03 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,8 @@
 
 # Directory containing files
 DIRECTORY="networks"
+ORIGINAL_NETWORKS="intradomain_wl.json intradomain.json routers_wl.json routers.json webserver_wl.json webserver.json"
+EXTEND_NETWORKS="datacenter_wl.json datacenter.json fattree_wl.json"
 
 # Array of elements
 OLLAMA_MODELS="llama2:7b llama2:13b mistral:7b-instruct-v0.2-q8_0 gemma2:27b llama3.1:8b"
@@ -12,24 +14,23 @@ OLLAMA_SCRIPT="src/network_model_ollama.py"
 OPENAI_SCRIPT="src/network_model_openai.py"
 
 # Outer loop: Iterate over files in the directory
-# for file in "$DIRECTORY"/*; do
-#     relative_file="${DIRECTORY}/$(basename "$file")"
+for file in $EXTEND_NETWORKS; do
+    relative_file="${DIRECTORY}/${file}"
     
-#     echo "Processing file: $relative_file"
-    
-#     # Inner loop: Iterate over array elements
-#     for element in $OLLAMA_MODELS; do
-#         echo "  Passing element: $element"
+    echo "Processing file: $relative_file"
+    # Inner loop: Iterate over array elements
+    for element in $OLLAMA_MODELS; do
+        echo "  Passing element: $element"
         
-#         # Pass arguments to the Python script
-#         # "network name", "model code"
-#         python3 "$OLLAMA_SCRIPT" "$relative_file" "$element"
-#     done
-# done
+        # Pass arguments to the Python script
+        # "network name", "model code"
+        python3 "$OLLAMA_SCRIPT" "$relative_file" "$element"
+    done
+done
 
 # Outer loop: Iterate over files in the directory
-for file in "$DIRECTORY"/*; do
-    relative_file="${DIRECTORY}/$(basename "$file")"
+for file in $EXTEND_NETWORKS; do
+    relative_file="${DIRECTORY}/${file}"
     
     echo "Processing file: $relative_file"
     
